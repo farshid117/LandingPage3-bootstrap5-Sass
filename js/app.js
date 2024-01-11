854ace2a5f6c3381ac425c786336e0ef3d480086
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-01-11 11:32:58 +0330

    implemetation Scroll event behavior on Header section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
+//todo:  nav menu toggle 
 const navMenu = document.querySelector(".nav__menu");
 const navToggle = document.querySelector(".nav__toggle");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("show-menu");
 });
+
+//todo: navbar scroll behavior 
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", () => {
+    // console.log("scroll:", window.scrollY)
+    if (window.scrollY > 30) {
+        nav.classList.add("nav-scroll");
+    } else {
+        nav.classList.remove("nav-scroll")
+    }
+}) 
